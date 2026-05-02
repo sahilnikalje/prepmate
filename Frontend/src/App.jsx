5dbc696ad9a12e1e27afb3159794ca426758be24
sahilnikalje <sahilnik88@gmail.com>
2026-05-03 00:03:54 +0530

    feat: add ProtectedRoute component for authentication and update API endpoints in authService

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,6 +7,7 @@ import PracticePage from './features/practice/pages/PracticePage';
 import AnalyticsPage from './features/dashboard/pages/AnalyticsPage';
 import ResourcesPage from './features/dashboard/pages/ResourcesPage';
 import InterviewPage from './features/interview/pages/InterviewPage';
+import ProtectedRoute from './features/auth/components/ProtectedRoute';
 
 function App() {
   return (
@@ -15,11 +16,12 @@ function App() {
           <Route path='/' element={<Navigate to='/login'/>}/>
           <Route path='/login' element={<LoginPage/>}/>
           <Route path='/signup' element={<SignupPage/>}/>
-          <Route path="/dashboard"  element={<DashboardPage/>} />
-          <Route path="/practice"   element={<PracticePage/>} />
-          <Route path="/analytics"  element={<AnalyticsPage/>} />
-          <Route path="/resources"  element={<ResourcesPage/>} />
-          <Route path='/interview/:id' element={<InterviewPage/>}/>
+          
+          <Route path="/dashboard"  element={<ProtectedRoute><DashboardPage/></ProtectedRoute>} />
+          <Route path="/practice"   element={<ProtectedRoute><PracticePage/></ProtectedRoute>} />
+          <Route path="/analytics"  element={<ProtectedRoute><AnalyticsPage/></ProtectedRoute>} />
+          <Route path="/resources"  element={<ProtectedRoute><ResourcesPage/></ProtectedRoute>} />
+          <Route path='/interview/:id' element={<ProtectedRoute><InterviewPage/></ProtectedRoute>}/>
        </Routes>
     </BrowserRouter>
   )
--- Frontend/src/features/auth/components/LoginForm.jsx
+++ Frontend/src/features/auth/components/LoginForm.jsx
@@ -107,7 +107,7 @@ function LoginForm() {
       {/*//* or divider */}
       <div className='relative flex items-center gap-4'>
         <div className='h-[1px] flex-1 bg-outline-variant/30'/>
-        <span text-xs font-bold text-outline-varient uppercase tracking-widest>Or Continue With</span>
+        <span className='text-xs font-bold text-outline-variant uppercase tracking-widest'>OR</span>
         <div className='h-[1px] flex-1 bg-outline-variant/30'/>
       </div>
 
@@ -122,7 +122,7 @@ function LoginForm() {
           <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="currentColor"/>
           <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="currentColor"/>
         </svg>
-        Google
+       Continue With Google
       </button>
     </form>
   )
