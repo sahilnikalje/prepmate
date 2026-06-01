0821a5b7b1ce5445d0aea0a377321d14ba3d6bee
sahilnikalje <sahilnik88@gmail.com>
2026-06-01 19:01:00 +0530

    fix: Update cookie settings for token to ensure compatibility across browsers

--- Backend/features/auth/auth.controller.js
+++ Backend/features/auth/auth.controller.js
@@ -11,8 +11,8 @@ const generateTokenAndSetCookie=(res,userId)=>{
 
     res.cookie('token', token,{
         httpOnly:true,
-        secure:process.env.NODE_ENV==='production',
-        sameSite:'lax',
+        secure:true,
+        sameSite:'none',
         maxAge: 7*24*60*60*1000
     })
 }
